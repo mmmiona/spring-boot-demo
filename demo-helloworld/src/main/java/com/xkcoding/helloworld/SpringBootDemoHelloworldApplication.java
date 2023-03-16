c9700fdcb3586ee8716417ac4364fac68895f1af
liu <954608891@qq.com>
2023-03-16 23:02:41 +0800

    本地项目集成jib插件

--- demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
+++ demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloworldApplication.java
@@ -32,7 +32,7 @@ public class SpringBootDemoHelloworldApplication {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(required = false, name = "who") String who) {
         if (StrUtil.isBlank(who)) {
-            who = "World5";
+            who = "World6";
         }
         return StrUtil.format("Hello, {}!", who);
     }
